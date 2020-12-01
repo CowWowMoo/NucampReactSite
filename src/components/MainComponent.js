0e4acc9007436b34ad43046d32d67c3dadd01b5b
CowWowMoo <kelseycgardner@gmail.com>
2020-12-01 13:56:25 -0800

    Workshop 3

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,54 +1,64 @@
 import React, { Component } from 'react';
 import Directory from './DirectoryComponent';
+import CampsiteInfo from './CampsiteInfoComponent';
 import { CAMPSITES } from '../shared/campsites';
-import { COMMENTS } from '../shared/comments';
-import { PARTNERS } from '../shared/partners';
-import { PROMOTIONS } from '../shared/promotions';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Home from './HomeComponent';
-import Contact from './ContactComponent';
-import CampsiteInfo from './CampsiteInfoComponent';
 import { Switch, Route, Redirect } from 'react-router-dom';
-
+import { BrowserRouter } from 'react-router-dom';
+import Contact from './ContactComponent';
+import { COMMENTS } from '../shared/comments';
+import { PARTNERS } from '../shared/partners';
+import { PROMOTIONS } from '../shared/promotions';
+import About from './AboutComponent';
 
 class Main extends Component {
-
-    constructor(props) {
-        super(props);
-        this.state = {
-            campsites: CAMPSITES,
-            comments: COMMENTS,
-            partners: PARTNERS,
-            promotions: PROMOTIONS
-        };
-    }
-
-    render() {
-        console.log(this.state.campsites.filter(campsite => campsite.featured)[0])
-        const HomePage = () => {
-            return (
-                <Home
-                    campsite={this.state.campsites.filter(campsite => campsite.featured)[0]}
-                    promotion={this.state.promotions.filter(promotion => promotion.featured)[0]}
-                    partner={this.state.partners.filter(partner => partner.featured)[0]}
-                />
-            );
-        };
-
-        return (
-            <div>
-                <Header />
-                <Switch>
-                    <Route path='/home' component={HomePage} />
-                    <Route exact path='/directory' render={() => <Directory campsites={this.state.campsites} />} />
-                    <Route exact path='/contactus' component={Contact} />
-                    <Redirect to='/home' />
-                </Switch>
-                <Footer />
-            </div>
-        );
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+        campsites: CAMPSITES,
+        comments: COMMENTS,
+        partners: PARTNERS,
+        promotions: PROMOTIONS
+    };
 }
+
+  render () {
+   
+    const HomePage = () => {
+      return (
+          <Home
+              campsite={this.state.campsites.filter(campsite => campsite.featured)[0]}
+              promotion={this.state.promotions.filter(promotion => promotion.featured)[0]}
+              partner={this.state.partners.filter(partner => partner.featured)[0]}
+          />
+      );
+    };
+        const CampsiteWithId =({match}) => {
+          return (
+            <CampsiteInfo
+                comapsite={this.state.campsites.filter(campsite => campsite.id === +match.params.campsiteId)[0]}
+                comments={this.state.comments.filter(comment => comment.campsiteid === +match.params.campsiteid)}
+                />
+          );
+        };
+  
+        return ( 
+              <div>
+                  <Header />
+                  <Switch>
+                      <Route path='/home' component={HomePage} />
+                      <Route exact path='/directory' render={() => <Directory campsites={this.state.campsites} />} />
+                      <Route exact path='/contactus' component={Contact} />
+                      <Route exact path='/aboutus' render={() => <About partners = {this.state.partners} />} />
+                      <Redirect to='/home' />
+                  </Switch>
+                  <Footer />
+              </div>
+          );
+              
+        }
+  }
 
 export default Main;
