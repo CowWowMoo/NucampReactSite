85029d94d0ed902c26e7c2fa7310594bc7d23659
CowWowMoo <kelseycgardner@gmail.com>
2020-12-08 14:54:28 -0800

    Combining Reducers

---
+++ src/redux/partners.js
@@ -0,0 +1,8 @@
+import { PARTNERS } from '../shared/partners';
+
+export const Campsites = (state = PARTNERS, action) => {
+    switch (action.type) {
+        default: 
+        return state;
+    }
+}; 
